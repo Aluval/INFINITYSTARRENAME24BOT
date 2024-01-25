1474746fe9b9ccb957e4550a025dd1f0721534b4
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-25 21:27:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,7 @@ API_ID = int(environ.get("API_ID", "10811400"))
 API_HASH = environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6487202001:AAHCk-mmxZ0wLTMUL4QYTqsXOjDuMCJQrTo")
 ADMIN = int(environ.get("ADMIN", "6469754522"))          
-CAPTION = environ.get("CAPTION", "<b>{filename}</b>
-
-♡ ㅤ   ❍ㅤ          ⎙           ⌲
- ˡᶦᵏᵉ    ᶜᵒᵐᵐᵉⁿᵗ    ˢᵃᵛᵉ     ˢʰᵃʳᵉ
-❍⋞◎◎◎◎◎◎◎◎◎◎◎◎◎◎◎◎◎◎◎⋟❍
-🌟 Jᴏɪɴ Cʜᴀɴɴᴇʟ: @sunriseseditsoffical6")
+CAPTION = environ.get("CAPTION", "🌟 Jᴏɪɴ Cʜᴀɴɴᴇʟ: @sunriseseditsoffical6")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"

b6d920f89a97752b3f147810b506a4baea3d5a72
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-25 21:43:38 +0530

    Update start_text.py

--- main/start_text.py
+++ main/start_text.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
-from config import ADMIN
+from config import ADMIN, SUDO_USERS
  
 
 @Client.on_message(filters.command("start") & filters.private)                             
@@ -11,14 +11,14 @@ async def start_cmd(bot, msg):
         ],[
         InlineKeyboardButton("UPDATES 📢", url="https://t.me/Sunrises24botupdates")
     ]])
-    if msg.from_user.id != ADMIN:
+    if msg.from_user.id != ADMIN, SUDO_USERS:
         return await msg.reply_text(text=txt, reply_markup=btn, disable_web_page_preview = True)
     await start(bot, msg, cb=False)
 
 
 @Client.on_callback_query(filters.regex("start"))
 async def start(bot, msg, cb=True):   
-    txt=f"hai {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://t.me/Sunrises24botupdates>SUNRISES ™</a></b>"                                     
+    txt=f"Hey...! {msg.from_user.mention} i am simple rename bot with personal usage.\nthis bot is made by <b><a href=https://t.me/Sunrises24botupdates>SUNRISES ™</a></b>"                                     
     button= [[
         InlineKeyboardButton("UPDATES 📢", url="https://t.me/Sunrises24botupdates")
         ],[

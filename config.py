ef9b02d2ac89fcaea8b09f0f5745403b7dcbbcdc
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-25 21:41:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from os import environ
 API_ID = int(environ.get("API_ID", "10811400"))
 API_HASH = environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6487202001:AAHCk-mmxZ0wLTMUL4QYTqsXOjDuMCJQrTo")
-ADMIN = int(environ.get("ADMIN", "6469754522"))          
+ADMIN = int(environ.get("ADMIN", "6469754522"))  
+SUDO_USERS = int(environ.get("SUDO_USERS", "6469754522, 6621835067"))
 CAPTION = environ.get("CAPTION", "🌟 Jᴏɪɴ Cʜᴀɴɴᴇʟ: @sunriseseditsoffical6")
 
 # for thumbnail ( back end is MrMKN brain 😉)

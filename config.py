307d19bb32e652b2c127956c6b245eeed23c495f
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 11:43:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "10811400"))
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAHCk-mmxZ0wLTMUL4QYTqsXOjDuMCJQrTo")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAFWEQ2RIaXQgV-83-74Oh2yF3T68WeJqCg")
 ADMIN = int(os.environ.get("ADMIN", "6469754522"))  
 CAPTION = os.environ.get("CAPTION", "")
 # for thumbnail ( back end is MrMKN brain 😉)

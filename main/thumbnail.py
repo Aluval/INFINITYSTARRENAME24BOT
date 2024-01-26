977d47b2934b0544e864e92e845406cf652419df
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 10:52:25 +0530

    Add files via upload

---
+++ main/thumbnail.py
@@ -0,0 +1,34 @@
+from pyrogram import Client, filters 
+from config import DOWNLOAD_LOCATION
+import os
+
+dir = os.listdir(DOWNLOAD_LOCATION)
+
+@Client.on_message(filters.private & filters.photo)                            
+async def set_tumb(bot, msg):       
+    if len(dir) == 0:
+        await bot.download_media(message=msg.photo.file_id, file_name=f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
+        return await msg.reply(f"Your permanent thumbnail is saved in dictionary ✅️ \nif you change yur server or recreate the server app to again reset your thumbnail⚠️")            
+    else:    
+        os.remove(f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
+        await bot.download_media(message=msg.photo.file_id, file_name=f"{DOWNLOAD_LOCATION}/thumbnail.jpg")               
+        return await msg.reply(f"Your permanent thumbnail is saved in dictionary ✅️ \nif you change yur server or recreate the server app to again reset your thumbnail⚠️")            
+
+
+@Client.on_message(filters.private & filters.command("view"))                            
+async def view_tumb(bot, msg):
+    try:
+        await msg.reply_photo(photo=f"{DOWNLOAD_LOCATION}/thumbnail.jpg", caption="this is your current thumbnail")
+    except Exception as e:
+        print(e)
+        return await msg.reply_text(text="you don't have any thumbnail")
+
+@Client.on_message(filters.private & filters.command(["del", "del_thumb"]))                            
+async def del_tumb(bot, msg):
+    try:
+        os.remove(f"{DOWNLOAD_LOCATION}/thumbnail.jpg")
+        await msg.reply_text("your thumbnail was removed🚫")
+    except Exception as e:
+        print(e)
+        return await msg.reply_text(text="you don't have any thumbnail")
+    

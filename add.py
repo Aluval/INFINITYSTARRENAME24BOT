17efb1acc7cd4ba0b4f8dbc8a1c34b566948d56b
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 10:38:22 +0530

    Create add.py

---
+++ add.py
@@ -0,0 +1,15 @@
+# Tg:@Sunrises_24
+
+from configs import Config
+from helper.database import db
+from pyrogram import Client
+
+
+async def add_user_to_database(bot, msg):
+    if not await db.is_user_exist(cmd.from_user.id):
+        await db.add_user(msg.from_user.id)
+        if Config.LOG_CHANNEL is not None:
+            await bot.send_flooded_message(
+                int(Config.LOG_CHANNEL),
+                f"#NEW_USER: \n\nNew User [{msg.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
+            )

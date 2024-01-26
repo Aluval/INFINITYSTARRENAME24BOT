4699af27b0ae385878f31dc99af92428c6478b09
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 10:55:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002112896731"))
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
 
 # for thumbnail ( back end is MrMKN brain 😉)
-#DOWNLOAD_LOCATION = "./DOWNLOADS"
+DOWNLOAD_LOCATION = "./DOWNLOADS"
 

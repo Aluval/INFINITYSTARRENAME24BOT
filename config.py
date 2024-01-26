1399f5d020de7024c1aa752c25912af4f13d7876
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 07:12:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = environ.get("BOT_TOKEN", "6487202001:AAHCk-mmxZ0wLTMUL4QYTqsXOjDuMCJQrTo")
 ADMIN = int(environ.get("ADMIN", "6469754522"))  
 SUDO_USERS = environ.get("SUDO_USERS", "6469754522, 6621835067")
-CAPTION = environ.get("CAPTION", "<b>{filename}</b>\n🌟 Jᴏɪɴ Cʜᴀɴɴᴇʟ: @sunriseseditsoffical6")
+CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"

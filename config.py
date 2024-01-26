1d732d11134a45f4d8e149f4fb3938bc2c3f6956
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 13:31:37 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 from os import environ
 import os
 
@@ -6,6 +7,6 @@ API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAFWEQ2RIaXQgV-83-74Oh2yF3T68WeJqCg")
 ADMIN = int(os.environ.get("ADMIN", "6469754522"))  
 CAPTION = os.environ.get("CAPTION", "")
-# for thumbnail ( back end is MrMKN brain 😉)
+ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 

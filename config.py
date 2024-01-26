1e9311f693ded47d1dcb6c13d299455025141bc7
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 07:26:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN", "6487202001:AAHCk-mmxZ0wLTMUL4QYTqsXOjDuMCJ
 ADMIN = int(environ.get("ADMIN", "6469754522"))  
 SUDO_USERS = environ.get("SUDO_USERS", "6469754522, 6621835067")
 CAPTION = environ.get("CAPTION", "")
+SUNRISES_PIC = os.environ.get("SUNRISES_PIC", "https://graph.org/file/c4d45a1ce85cfa956d3da.jpg") 
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"

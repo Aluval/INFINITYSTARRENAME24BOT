20e3cf3c22804ca6bf6fe49ca91cdac841d9fccb
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 10:26:31 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -1,38 +1,75 @@
-import pymongo
-import os
-from config import *
+# Tg:@Sunrises_24
 
-mongo = pymongo.MongoClient(DB_URL)
-
-db = mongo[DB_NAME]
-dbcol = db["user"]
-
-#def addthumb(chat_id, file_id):
-    #dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": file_id}})
+import datetime
+import motor.motor_asyncio
+from configs import Config
 
 
-#def delthumb(chat_id):
-    #dbcol.update_one({"_id": chat_id}, {"$set": {"file_id": None}})
+class Database:
+    def __init__(self, uri, database_name):
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+        self.db = self._client[database_name]
+        self.col = self.db.users
+
+    def new_user(self, id):
+        return dict(
+            id=id,
+            join_date=datetime.date.today().isoformat(),
+            apply_caption=True,
+            upload_as_doc=False,
+            thumbnail=None,
+            caption=None
+        )
+
+    async def add_user(self, id):
+        user = self.new_user(id)
+        await self.col.insert_one(user)
+
+    async def is_user_exist(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return bool(user)
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+    async def get_all_users(self):
+        return self.col.find({})
+
+    async def delete_user(self, user_id):
+        await self.col.delete_many({'id': int(user_id)})
+
+    async def set_apply_caption(self, id, apply_caption):
+        await self.col.update_one({'id': id}, {'$set': {'apply_caption': apply_caption}})
+
+    async def get_apply_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('apply_caption', True)
+
+    async def set_upload_as_doc(self, id, upload_as_doc):
+        await self.col.update_one({'id': id}, {'$set': {'upload_as_doc': upload_as_doc}})
+
+    async def get_upload_as_doc(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('upload_as_doc', False)
+
+    async def set_thumbnail(self, id, thumbnail):
+        await self.col.update_one({'id': id}, {'$set': {'thumbnail': thumbnail}})
+
+    async def get_thumbnail(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('thumbnail', None)
+
+    async def set_caption(self, id, caption):
+        await self.col.update_one({'id': id}, {'$set': {'caption': caption}})
+
+    async def get_caption(self, id):
+        user = await self.col.find_one({'id': int(id)})
+        return user.get('caption', None)
+
+    async def get_user_data(self, id) -> dict:
+        user = await self.col.find_one({'id': int(id)})
+        return user or None
 
 
-def addcaption(chat_id, caption):
-    dbcol.update({"_id": chat_id}, {"$set": {"caption": caption}})
-
-def delcaption(chat_id):
-    dbcol.update({"_id": chat_id}, {"$set": {"caption": None}})
-
-
-def find(chat_id):
-    id = {"_id": chat_id}
-    x = dbcol.find(id)
-    for i in x:
-        file = i["file_id"]
-        try:
-            caption = i["caption"]
-        except:
-            caption = None
-        return [file, caption]
-
-
-
-
+db = Database(Config.MONGODB_URI, "Rename-Bot")

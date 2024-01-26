d49d06b6948b5c48f76a0ba41cf49cc8c38a24d3
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 07:28:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 from os import environ
+import os
 
-API_ID = int(environ.get("API_ID", "10811400"))
-API_HASH = environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6487202001:AAHCk-mmxZ0wLTMUL4QYTqsXOjDuMCJQrTo")
-ADMIN = int(environ.get("ADMIN", "6469754522"))  
-SUDO_USERS = environ.get("SUDO_USERS", "6469754522, 6621835067")
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(os.environ.get("API_ID", "10811400"))
+API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAHCk-mmxZ0wLTMUL4QYTqsXOjDuMCJQrTo")
+ADMIN = int(os.environ.get("ADMIN", "6469754522"))  
+SUDO_USERS = os.environ.get("SUDO_USERS", "6469754522, 6621835067")
+CAPTION = os.environ.get("CAPTION", "")
 SUNRISES_PIC = os.environ.get("SUNRISES_PIC", "https://graph.org/file/c4d45a1ce85cfa956d3da.jpg") 
 
 # for thumbnail ( back end is MrMKN brain 😉)

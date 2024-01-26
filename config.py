23b9d45f8e93524391ac5ac8cc81779c3dc77f18
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 10:50:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ CAPTION = os.environ.get("CAPTION", "")
 DB_NAME = os.environ.get("DB_NAME","Sunrises_24")     
 DB_URL = os.environ.get("DB_URL","mongodb+srv://RAINBOWRENAME24BOT:<password>@cluster0.ud3toaz.mongodb.net/?retryWrites=true&w=majority")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002112896731"))
-    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
 
 # for thumbnail ( back end is MrMKN brain 😉)
 #DOWNLOAD_LOCATION = "./DOWNLOADS"

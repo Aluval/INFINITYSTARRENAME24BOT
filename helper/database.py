3931dbe09a8fa36ceee1c986e1daa7359b6addeb
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 10:33:57 +0530

    Update database.py

--- helper/database.py
+++ helper/database.py
@@ -14,9 +14,7 @@ class Database:
     def new_user(self, id):
         return dict(
             id=id,
-            join_date=datetime.date.today().isoformat(),
-            apply_caption=True,
-            upload_as_doc=False,
+            join_date=datetime.date.today().isoformat(),       
             thumbnail=None,
             caption=None
         )

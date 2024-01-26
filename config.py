f29a77fdfd4e008d44f1514f653de2aa4beda8c6
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 13:31:58 +0530

    ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAFWEQ2RIaXQgV-83-74Oh2yF3T68WeJqCg")
 ADMIN = int(os.environ.get("ADMIN", "6469754522"))  
 CAPTION = os.environ.get("CAPTION", "")
-ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
+#ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 

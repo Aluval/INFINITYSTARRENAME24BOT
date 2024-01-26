31e3b68c97065829b1fe7f64b31682a1b499f340
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 09:01:35 +0530

    Update caption.py

--- main/caption.py
+++ main/caption.py
@@ -1,5 +1,6 @@
 from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from helper.database import *
 from config import *
 
 @Client.on_message(filters.private & filters.command('set_caption'))

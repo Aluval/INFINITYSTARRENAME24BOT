3d7f3e52cf3fc1a8bfda4447bfe20926b6c40a77
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-26 09:29:50 +0530

    Update caption.py

--- main/caption.py
+++ main/caption.py
@@ -13,13 +13,15 @@ async def add_caption(bot, msg):
 
 @Client.on_message(filters.private & filters.command('del_caption'))
 async def delete_caption(bot, msg): 
-    caption = find(int(msg.chat.id))[1]
-    if not caption:
-        await msg.reply_text("**yᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴy ᴄᴏꜱᴛᴏᴍ ᴄᴀᴩᴛɪᴏɴ !!**")
+    result = find(int(msg.chat.id))
+    if result is None or not result[1]:
+        await msg.reply_text("**yᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴy ᴄuꜱᴛᴏᴍ ᴄᴀᴩᴛɪᴏɴ !!**")
         return
     delcaption(int(msg.chat.id))
     await msg.reply_text("**yᴏᴜʀ ᴄᴀᴩᴛɪᴏɴ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟʟy ᴅᴇʟᴇᴛᴇᴅ !!**")
-                                      
+
+
+
 @Client.on_message(filters.private & filters.command('see_caption'))
 async def see_caption(bot, msg): 
     result = find(int(msg.chat.id))
@@ -27,6 +29,6 @@ async def see_caption(bot, msg):
         caption = result[1]
         await msg.reply_text(f"<b><u>Your Caption:</b></u>\n\n`{caption}`")
     else:
-        await msg.reply_text("**yᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴy ᴄᴏꜱᴛᴏᴍ ᴄᴀᴩᴛɪᴏɴ !!**")
+        await msg.reply_text("**yᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴy ᴄusᴛᴏᴍ ᴄᴀᴩᴛɪᴏɴ !!**")
 
           

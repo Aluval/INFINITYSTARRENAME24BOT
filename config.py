8dd2c1973fda1c92c2a4c7f42f60eb445cc33709
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-03-26 19:38:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ import os
 API_ID = int(os.environ.get("API_ID", "10811400"))
 API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6487202001:AAFWEQ2RIaXQgV-83-74Oh2yF3T68WeJqCg")
-ADMIN = int(os.environ.get("ADMIN", "6469754522"))  
+ADMIN = int(os.environ.get("ADMIN", "6469754522")) 
+FSUB_CHANNEL = os.environ.get("FSUB_CHANNEL")
 CAPTION = os.environ.get("CAPTION", "")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
